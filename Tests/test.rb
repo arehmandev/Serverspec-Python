75e3a790ee6efa2e827c59458fc126ad759d4aae
Abdul Rehman <arehmandev@outlook.com>
2017-06-16 14:21:09 +0100

    Initial commit

---
+++ Tests/test.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+# This is a sample test
+describe "World" do
+  describe command('echo World') do
+    its(:stdout) { should match 'World' }
+  end
+end
---
+++ Tests/test2.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+# This is a sample test
+describe "Hello" do
+  describe command('echo Hello') do
+    its(:stdout) { should match 'Hello' }
+  end
+end
